2a2f113441ff9a981bc116ccc1303eb3d4ac26cf
Madicke <cissemadicke8@gmail.com>
2025-04-02 17:48:24 +0200

    Add Toggle component for theme switching functionality

---
+++ src/components/toggle.jsx
@@ -0,0 +1,11 @@
+
+
+const Toggle= () => {
+    return(
+        <div>
+            <input type="checkbox" defaultChecked className="toggle toggle-primary" />
+        </div>
+    )
+}
+
+export default Toggle;
