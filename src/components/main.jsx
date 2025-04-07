af4238a6b0b530b6c876740efc1b1cc08a8dfab9
Madicke <cissemadicke8@gmail.com>
2025-04-07 15:54:15 +0200

    Update Main component to use user data from AuthContext for profile information

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
 import ProfilePage from './profile';
+import { useAuth } from '../authContext';
+
 
 const Main = () => {
-
+const {user} =useAuth();
   const [profile, setProfile] = useState({
-    name: 'Jenny Wilson',
-    email: 'j.wilson@example.com',
+    name: user.first_name,
+    email: user.email,
     description: 'Product designer with 5+ years of experience in UX/UI design.',
     image: '/api/placeholder/100/100'
   });
 
  
-
+  
+  console.log(user);
+ 
   return (
     <>
     <ProfilePage profile={profile} />
