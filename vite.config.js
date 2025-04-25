6a93035b4c996318269615a382cc014ef07906e5
Madicke Cisse <cissemadicke8@gmail.com>
2025-04-25 17:56:07 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+    server: {
+    host: '0.0.0.0', // so it binds to public IP
+    port: 5173,      // or whatever port you're using
+    allowedHosts: ['ec2-44-244-208-132.us-west-2.compute.amazonaws.com']
+  }
+  
 });
